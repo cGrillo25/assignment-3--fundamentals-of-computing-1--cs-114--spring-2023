10ffd510d28adbaa384a41d332c9e1300cf05edc
Courtney Grillo <cgrillo@hartford.edu>
2023-04-05 16:02:14 -0400

    Cleaning
    Added appropriate spaces and brackets.

--- Matrix.java
+++ Matrix.java
@@ -6,50 +6,50 @@
 
 public class Matrix {
   private int [][]matrix;
-  private void swap(int x1, int y1, int x2, int y2){
+  private void swap(int x1, int y1, int x2, int y2) {
     int storage = 0;
     storage = matrix[x1][y1];
     matrix[x1][y1] = matrix[x2][y2];
     matrix[x2][y2] = storage;
   }
 
-  public Matrix(int size){
+  public Matrix(int size) {
     matrix = new int [size][size];
     System.out.println();
   }
 
-  public void populateDefaultMatrix(){
-    for (int row = 0; row < matrix.length; row++){
-      for (int column = 0; column < matrix.length; column++){
+  public void populateDefaultMatrix() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix.length; column++) {
         matrix[row][column] = (0);
       }
     }
   }
 
-  public void populateMatrix(){
+  public void populateMatrix() {
     int cell = 1;
 
-    for (int row = 0; row < matrix.length; row++){
-      for (int column = 0; column < matrix.length; column++){
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix.length; column++) {
         matrix[row][column] = (cell);
         cell ++;
       }
     }
   }
 
-  public void printDefaultMatrix(){
+  public void printDefaultMatrix() {
     System.out.println();
 
     int xTracker = 0;
     int yTracker = matrix.length-1;
 
-    for (int row = 0; row < matrix.length; row++){
-      for (int column = 0; column < matrix[row].length; column++){
-        if(row == xTracker && column == yTracker){
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix[row].length; column++) {
+        if(row == xTracker && column == yTracker) {
           System.out.print("\u001B[33m\t" + matrix[row][column]);
+        } else {
+          System.out.print("\u001B[0m\t" + matrix[row][column]);
         }
-        else
-        System.out.print("\u001B[0m\t" + matrix[row][column]);
       }
       xTracker++;
       yTracker--;
@@ -57,19 +57,19 @@ public class Matrix {
     }
   }
 
-  public void printMatrix(){
+  public void printMatrix() {
     System.out.println();
 
     int xTracker = 0;
     int yTracker = matrix.length-1;
 
-    for (int row = 0; row < matrix.length; row++){
-      for (int column = 0; column < matrix[row].length; column++){
-        if(row == xTracker && column == yTracker){
+    for (int row = 0; row < matrix.length; row++) {
+      for (int column = 0; column < matrix[row].length; column++) {
+        if(row == xTracker && column == yTracker) {
           System.out.print("\u001B[33m\t" + matrix[row][column]);
+        } else {
+          System.out.print("\u001B[0m\t" + matrix[row][column]);
         }
-        else
-        System.out.print("\u001B[0m\t" + matrix[row][column]);
       }
       xTracker++;
       yTracker--;
@@ -77,11 +77,11 @@ public class Matrix {
     }
   }
 
-  public void flipMatrix(){
+  public void flipMatrix() {
     int newRow = matrix.length;
 
-    for(int row = 0; row < newRow; row++){
-      for(int column = 0; column < (newRow -1) - row; column++){
+    for(int row = 0; row < newRow; row++) {
+      for(int column = 0; column < (newRow -1) - row; column++) {
         swap(row, column, newRow -1 -column, newRow -1 -row);
       }
     }
